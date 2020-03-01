07d37095386c0da5480e2bbd0bba0c9a7e856239
anil kumar kothapalli <anilkumar1988@gmail.com>
2020-03-01 21:28:29 +0100

    Sudoku Algorithm implemented using recursion and lookup

---
+++ FileHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace Sudoku
+{
+    class FileHandler
+    {
+        static public char[,] GetSudokuGrid()
+        {
+            string path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            var directory = System.IO.Path.GetDirectoryName(path);
+            var fileName = UI.GetFilenameFromUser();
+
+            List<string> sudokuRows = new List<string>();
+            using (StreamReader reader = new StreamReader(directory + @"\" + fileName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    sudokuRows.Add(reader.ReadLine());
+                }
+            }
+
+            var result = sudokuRows.Select(item => item.ToCharArray()).ToArray();
+
+            if(result.GetLength(0) == 9)
+            {
+                return JaggedArrayTo2D(result);
+            }
+            return new char[,] { };
+
+
+            
+        }
+
+        static char[,] JaggedArrayTo2D(char[][] jaggedArray)
+        {
+            char[,] result = new char[jaggedArray.Length, jaggedArray[0].Length];
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int k = 0; k < jaggedArray[0].Length; k++)
+                {
+                    result[i, k] = jaggedArray[i][k];
+                }
+            }
+
+            return result;
+        }
+    }
+}
