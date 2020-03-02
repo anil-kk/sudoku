6f46b69f271301b0bb6b59803e3d4db9059b1ab2
anil kumar kothapalli <anilkumar1988@gmail.com>
2020-03-02 21:49:13 +0100

    Sudoku puzzle creation and possibility to persist result in a file promted by the user

--- FileHandler.cs
+++ FileHandler.cs
@@ -12,31 +12,66 @@ namespace Sudoku
         {
             string path = System.Reflection.Assembly.GetExecutingAssembly().Location;
             var directory = System.IO.Path.GetDirectoryName(path);
-            var fileName = UI.GetFilenameFromUser();
+            var fileName = UI.GetFilenameFromUser("Enter file name to load: (ex: sudoku.txt)");
 
             List<string> sudokuRows = new List<string>();
 
             try { 
-            using (StreamReader reader = new StreamReader(directory + @"\" + fileName))
-            {
-                while (!reader.EndOfStream)
+                using (StreamReader reader = new StreamReader(directory + @"\" + fileName))
                 {
-                    sudokuRows.Add(reader.ReadLine());
+                    while (!reader.EndOfStream)
+                    {
+                        sudokuRows.Add(reader.ReadLine());
+                    }
                 }
-            }
 
-            var result = sudokuRows.Select(item => item.ToCharArray()).ToArray();
+                var result = sudokuRows.Select(item => item.ToCharArray()).ToArray();
 
-            if(result.GetLength(0) == 9 && result[0].Length == 9)
-            {
-                return JaggedArrayTo2D(result);
-            }
-            return new char[,] { };
+                if(result.GetLength(0) == 9 && result[0].Length == 9)
+                {
+                    return JaggedArrayTo2D(result);
+                }
+                return new char[,] { };
             }
 
             catch
             {
                 return new char[,] { };
+            }
+        }
+
+        static public bool PersistSudokuGrid(char[,] grid)
+        {
+            if (!UI.UserPromt())
+            {
+                return false;
+            }
+            string path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            var directory = System.IO.Path.GetDirectoryName(path);
+            var fileName = UI.GetFilenameFromUser("Enter file name to save: (ex: sudoku.txt)");
+
+            try
+            {
+                using (var writer = new StreamWriter(directory + @"\" + fileName))
+                {
+                    for (int row = 0; row < grid.GetLength(0); row++)
+                    {
+                        for (int col = 0; col < grid.GetLength(1); col++)
+                        {                        
+                            writer.Write(grid[row, col]);
+                            if (col == 8)
+                            {
+                                writer.WriteLine("");
+                            }                                
+                        }
+                    }
+                }
+                return true;
+            }
+
+            catch
+            {
+                return false;
             }
         }
 
